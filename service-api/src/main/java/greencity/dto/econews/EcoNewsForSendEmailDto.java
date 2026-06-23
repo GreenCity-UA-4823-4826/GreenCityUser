64b1e34de2b8c9613add2878e80c066cbe409087
cherishhh <110904182+IrynaVdovych@users.noreply.github.com>
2026-06-23 17:41:56 +0300

    Update EcoNewsForSendEmailDto.java

--- service-api/src/main/java/greencity/dto/econews/EcoNewsForSendEmailDto.java
+++ service-api/src/main/java/greencity/dto/econews/EcoNewsForSendEmailDto.java
@@ -2,7 +2,6 @@ package greencity.dto.econews;
 
 import greencity.dto.user.PlaceAuthorDto;
 import java.time.ZonedDateTime;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;

e640af326042f0447cce374ff60390bd27adaeda
EHESPO International nonprofit corporation <executivedirector@eheps.org>
2026-07-09 22:14:21 -0700

    Add contact details and organization domains
    Added contact information and organization domains to SecurityConfig.java.

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -263,5 +263,13 @@ public class SecurityConfig {
             .setAudience(audiences)
             .setAcceptableTimeSkewSeconds(120)
             .build();
-    }
-}
+
+Executive Director
+executive@eheps.com
+
+System Administrator
+admin@eheps.com
+
+Organization Domains
+https://eheps.com
+https://eheps.org
